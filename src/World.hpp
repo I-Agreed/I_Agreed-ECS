72f178791b957e8d29d3949dee01181aa7b93d8a
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2022-01-21 17:04:27 +1100

    fixed entity removal

--- src/World.hpp
+++ src/World.hpp
@@ -6,6 +6,8 @@
 namespace IA::ECS {
     struct World {
         std::vector<Entity*> entities; // Entities within this world
+        std::vector<Entity*> entitiesToRemove; // Entities marked for removal
+
         std::vector<System*> systems; // Entities within this world
 
         // Add an entity to this world
@@ -21,7 +23,7 @@ namespace IA::ECS {
         void remove_entity(Entity* entity) {
             for (System* system:systems) {
                 if (system->check(entity)) {
-                    system->entities.push_back(entity);
+                    entitiesToRemove.push_back(entity);
                 }
             }
         }
@@ -35,10 +37,20 @@ namespace IA::ECS {
             }
         }
 
+        // Removes all entities marked for removal
+        void remove_marked_entities() {
+            for (Entity* entity:entitiesToRemove) {
+                actually_remove_entity(entity);
+            }
+            entitiesToRemove.clear();
+        }
+
+        // Calls update_all on each system, then removes entities.
         void update() {
             for (System* system:systems) {
                 system->update_all();
             }
+            remove_marked_entities();
         }
     };
 }
